89702a8696f4b235ce03ff61da7e4624d184c16b
saptarshithe42 <saptarshi.the.42@gmail.com>
2024-09-19 20:29:46 +0530

    year update

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import profile_img from "../images/profile_pic.jfif"
+import profile_img from "../images/profile_pic.jfif";
 
 function Introduction() {
-
     return (
-
         <div className="container-fluid intro">
             <div className="row">
                 <div className="col-12 col-xl-6 about">
                     <h1 className="name-heading">Hi! I am Saptarshi Mondal</h1>
                     <p className="about">
-                        I am a Computer Science And Engineering Student from Jadavpur University, West Bengal, India. I am interested in exploring and working with new technologies.
-                        Currently I am working on my Web Development skills with the MERN stack. I am also looking forward to mobile application development, devops and many others. Currently I am in the Third year of my four year BE course.
-                        <br/>
+                        I am a Computer Science And Engineering Student from
+                        Jadavpur University, West Bengal, India. I am interested
+                        in exploring and working with new technologies.
+                        Currently I am working on my Web Development skills with
+                        the MERN stack. I am also looking forward to mobile
+                        application development, devops and many others.
+                        Currently I am in the Final year of my four year BE
+                        course.
                         <br />
-                        <a href={"mailto:saptarshi.the.42@gmail.com"} className="btn btn-outline-primary btn-lg" style={{ color: "white" }} target="_blank">Email : saptarshi.the.42@gmail.com </a>
+                        <br />
+                        <a
+                            href={"mailto:saptarshi.the.42@gmail.com"}
+                            className="btn btn-outline-primary btn-lg"
+                            style={{ color: "white" }}
+                            target="_blank"
+                        >
+                            Email : saptarshi.the.42@gmail.com{" "}
+                        </a>
                     </p>
                 </div>
                 <div className="col-12 col-xl-6 profile-pic-div">
@@ -22,9 +33,7 @@ function Introduction() {
                 </div>
             </div>
         </div>
-
-    )
-
+    );
 }
 
 export default Introduction;
